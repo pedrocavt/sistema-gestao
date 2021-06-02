24f0c255258e7c464aafbcdfa80795fd1db684a0
Pedro Vitor <pedrocavt@gmail.com>
2021-06-02 09:56:10 -0300

    avançando em rotas

--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -1,1 +1,8 @@
 <h3>Sobre nos</h3>
+
+
+<ul>
+    <li><a href="{{ route('site.index') }}">Principal</a></li>
+    <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
+    <li><a href="{{ route('site.contato') }}">Contato</a></li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){ return 'Login';})->name('site.login');
 
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){ return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){ return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){ return 'Produtos';})->name('app.produtos');
+});
 
-Route::get('/contato', 'ContatoController@contato');
+Route::fallback(function(){
+    echo "A rota acessada não existe, <a href='". route('site.index') ."'>clique aqui</a>" ;
+});
